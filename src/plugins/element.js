a7dad1eecc2badcb311684462ff160ab5b5e9824
shenwendi <55@qq.com>
2019-12-20 18:56:42 +0800

    完成了登录

---
+++ src/plugins/element.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import { Button } from 'element-ui'
+// import { Form, FormItem } from 'element-ui'
+// import { Input } from 'element-ui'
+
+Vue.use(Element)
+// Vue.use(Button)
+// Vue.use(Form)
+// Vue.use(FormItem)
+// Vue.use(Input)
